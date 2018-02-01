0dfca73753927d5592d56f675e99464f3521398f
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-02-01 16:27:44 +0530

    fixed point in an array in which duplicate is allowed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include<queue>
 using namespace std;
-int fixedpoint(int *array,int start,int last)
+/******************find fixed point in an array
+int fixedpointInDuplicateAllowed(int *array,int start,int last)
 {
     if(start>last)
         return INT_MIN;
@@ -17,7 +18,26 @@ int fixedpoint(int *array,int start,int last)
             return fixedpoint(array,start,mid-1);
     }
 }
-/******************find fixed point in an array*///////
+*///////
+/***********fixed point in an array in which duplicate is allowed*///////
+int fixedPointDuplicate(int *array,int start,int last)
+{
+    if(start>last)
+        return INT_MIN;
+    else
+    {
+        int mid=(start+last)/2;
+        if(array[mid]==mid)
+            return mid;
+        int left=min(array[mid],mid-1);
+        int leftfind=fixedPointDuplicate(array,start,left);
+        if(leftfind!=INT_MIN)
+            return leftfind;
+        int right=max(array[mid],mid+1);
+        int rightfind=fixedPointDuplicate(array,right,last);
+        return rightfind;
+    }
+}
 int main()
 {
     int n;
@@ -25,6 +45,6 @@ int main()
     int *array=new int[n];
     for(int i=0;i<n;i++)
         cin>>array[i];
-    cout<<fixedpoint(array,0,n-1);
+    cout<<fixedPointDuplicate(array,0,n-1);
     delete[] array;
 }
